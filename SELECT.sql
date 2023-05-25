4324225d1d59273d8135ede4a7acd3ed4ace130b
sva-ru <125918021+sva-ru@users.noreply.github.com>
2023-05-25 23:59:41 +0300

    change finish

--- SELECT.sql
+++ SELECT.sql
@@ -5,17 +5,17 @@
 select name, duration
 from track
 order by duration desc 
-limit 1
+limit 1;
 
 -- Название треков, продолжительность которых не менее 3,5 минут
 select name
 from track
-where duration >= 3.5 * 60
+where duration >= 3.5 * 60;
 
 -- Названия сборников, вышедших в период с 2018 по 2020 год включительно
 select name
 from collection
-where year_of_release between 2018 and 2020
+where year_of_release between 2018 and 2020;
 
 -- Исполнители, чьё имя состоит из одного слова
 select name
@@ -33,17 +33,17 @@ WHERE string_to_array(lower(name), ' ')  && ARRAY['my','мой'];
 select title g, count(a.name)
 from genre g left join genre_artist ga on ga.genres_id  = g.id 
 left join artist a on a.id = ga.artist_id
-group by g
+group by g;
 
 -- Количество треков, вошедших в альбомы 2019–2020 годов
 select count(t.name)
 from track t join album a on t.album_id = a.id
-where a.year_of_release between 2019 and 2020
+where a.year_of_release between 2019 and 2020;
 
 -- Средняя продолжительность треков по каждому альбому
 select a.title, avg(t.duration)
 from album a join track t on a.id  = t.album_id
-group by a.title
+group by a.title;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году
 select a.name
@@ -61,4 +61,4 @@ from collection c join track_collection tc on c.id = tc.collection_id
 join track t on t.id = tc.track_id join artist_album aa on aa.album_id  = t.album_id
 join artist a on a.id  = aa.artist_id
 where a.name = 'Billie Eilish'
-group by c."name" 
+group by c."name";
